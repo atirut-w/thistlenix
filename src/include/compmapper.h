2024ce79840e950a1f868cfc64d30376e8f12a65
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2022-02-20 22:28:45 +0700

    More consistent newlines

--- src/include/compmapper.h
+++ src/include/compmapper.h
@@ -3,21 +3,25 @@
 
 // Retrieve the status of the last executed command
 char get_cmd_status();
+
 // Execute a command
 void exec_cmd(char cmd);
 
 // Get command info
 char get_cmd_info();
+
 // Set component selector
 void set_component_sel(char sel);
 
 // Read data from command result buffer
 char read_result_buffer();
+
 // Write TSF(Thistle Serialized Format) data into command arguments buffer
 void write_buffer(char data);
 
 // Get current bank mask
 char get_bank_mask();
+
 // Modify memory bank masking
 void set_bank_mask(char mask);
 
