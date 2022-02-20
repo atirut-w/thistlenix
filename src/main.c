feb239e592d684b7b14ef3d30256971c9b075fdc
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2022-02-21 00:43:31 +0700

    Codes to test component mapper

--- src/main.c
+++ src/main.c
@@ -1,5 +1,18 @@
+#include <stdlib.h>
 #include "include/utils.h"
+#include "include/compmapper.h"
 
 void main() {
-    print("Hello, world!\n");
+    char tmpbuf[64];
+
+    print("Listing components...\n");
+    exec_cmd(3);
+
+    if (get_cmd_status() != 0x0) {
+        print("Bad IO\n");
+    }
+
+    print("Component count: ");
+    itoa(get_cmd_info(), tmpbuf, 10);
+    print(tmpbuf);
 }
