eee28e98544926ef765627ca53203caa21f1c585
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2022-02-21 01:15:36 +0700

    Component struct and prototype for `list_components()`
    I hope the UUID type is right...

--- src/include/component.h
+++ src/include/component.h
@@ -1,4 +1,12 @@
 #ifndef COMPONENT_H
 #define COMPONENT_H
 
+struct component {
+    char uuid[16];
+    char name[32];
+};
+
+// List connected components
+struct component *list_components();
+
 #endif
