73fa812f9d4da880e03af104e4103f374280c905
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2022-02-21 18:14:26 +0700

    Scrap a little more

--- src/main.c
+++ src/main.c
@@ -1,22 +1,6 @@
 #include <stdlib.h>
 #include "include/utils.h"
-#include "include/compmapper.h"
-#include "include/component.h"
 
 void main() {
-    print("Listing components...\n");
-
-    {
-        char i, component_count, tmpbuffer[16];
-        struct component *components;
-
-        components = list_components();
-        component_count = get_component_count();
-
-        for (i = 0; i < component_count; i++) {
-            print("Component ");
-            print(components[i].name);
-            print("\n");
-        }
-    }
+    
 }
