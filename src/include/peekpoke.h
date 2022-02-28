f52086f2fb0d8350fe9610b75bed5f1a3e4b598a
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2022-03-01 04:44:34 +0700

    Remove C source files

--- src/include/peekpoke.h
+++
@@ -1,7 +0,0 @@
-#ifndef _PEEKPOKE_H
-#define _PEEKPOKE_H
-
-#define PEEK(addr) (*(unsigned char *)(addr))
-#define POKE(addr, data) (*(unsigned char *)(addr) = (data))
-
-#endif
--- src/print.c
+++
@@ -1,8 +0,0 @@
-#include "print.h"
-#include "peekpoke.h"
-
-void print(const char *msg) {
-    while (*msg) {
-        POKE(0xE003, *msg++);
-    }
-}
