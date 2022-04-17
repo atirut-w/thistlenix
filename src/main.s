834d800f65ba586e333bcb78d36ea4093e4925e0
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2022-04-17 15:07:55 +0700

    Explicit segments

--- src/main.s
+++ src/main.s
@@ -1,9 +1,14 @@
+.segment "CODE"
+
+; Subroutines
 .include "subroutines/print.s"
+
+.segment "RODATA"
+
+text:
+    .byte "Hello World!", $0a, 0
 
 .segment "STARTUP"
     ldx #<text
     jsr print
     rts
-
-text:
-    .byte "Hello World!", $0a, 0
