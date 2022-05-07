90d41e428ad0c46325c21cfe3555a1dd871529bc
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2022-05-07 23:16:26 +0700

    Use IRQ and disable interrupts then handling

--- src/main.c
+++ src/main.c
@@ -2,10 +2,8 @@
 #include "basicio.h"
 
 void irq() {
-    __asm__("rti");
-}
+    __asm__("sei"); // Disable interrupts while we're in here
 
-void nmi() {
     print("Current uptime in ticks: $");
     printhex(PEEK(0xe059));
     printhex(PEEK(0xe058));
@@ -15,20 +13,19 @@ void nmi() {
     POKE(0xe05a, 1);
     POKE(0xe05b, 0);
 
-    __asm__("rti");
+    __asm__("cli"); // Enable interrupts again
+    __asm__("rti"); // Done
 }
 
 void main() {
-    // Set up IRQ and NMI handlers
+    // Set up IRQ handler
     POKE(0xfffe, (unsigned short)irq & 0xff);
     POKE(0xffff, (unsigned short)irq >> 8);
-    POKE(0xfffa, (unsigned short)nmi & 0xff);
-    POKE(0xfffb, (unsigned short)nmi >> 8);
 
-    // Set up timer to trigger NMI every tick(1/20 s)
+    // Set up timer to trigger IRQ every tick(1/20 s)
     POKE(0xe05a, 1);
     POKE(0xe05b, 0);
-    POKE(0x0e05f, 1);
+    POKE(0x0e05e, 1);
 
     print("OK\n");
 }
